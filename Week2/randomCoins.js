c40b6245a5ae723170d13079dc6d5615e679bb2a
ilikealotgithub1234 <erikpmurdock@gmail.com>
2016-04-04 08:22:04 -0400

    Update randomCoins.js

--- Week2/randomCoins.js
+++ Week2/randomCoins.js
@@ -1,16 +1,19 @@
 var button = $('randomButton')
-var coins{
-  nickel:5,
-  quarter:25,
-  penny:1;
-}
+var coins = [
+  {penny:1},
+  {nickel:5},
+  {dime:10},
+  {quarter:25}
+];
+/*
 var rndmFrmArryTObject = function(object){
-  var
+  var array = [];
   var randomNum = random(0,object.length);
   if(randomNum = 0){
-    array[];
+    console.log('invalid');
   }
-  var array = [];
+
+
   return array;
 };
 
@@ -19,3 +22,22 @@ $('button').on('click',function(){
 
 })
 console.log('hello world');
+*/
+
+var chooseRandomCoins = function(array){
+  var arrayOfRandom = [];
+  var length = array.length;
+  //if the array is ␀ (U+2400)
+  if (length =< 0){
+    console.log('invalid');
+
+  }
+  else{
+    var x = 0
+    for(x;x<length;x++)
+    rand = random(0,length);
+    var randomCoin = array[rand];
+    arrayOfRandom[]
+    return arrayOfRandom[];
+  }
+}
