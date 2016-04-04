063ccc02df7f12f7d21155fbe3ec20c275c5b989
ilikealotgithub1234 <erikpmurdock@gmail.com>
2016-04-04 19:53:37 -0400

    Update script.js

--- Week2/sparkfunhighscores/script.js
+++ Week2/sparkfunhighscores/script.js
@@ -1,1 +1,18 @@
+var get$ = $('#get');
+var po$t = $('#post');
+var priv_key = 'JqyGxekAvyT6dKe4BaAX';
+var name = 'erik';
+var score = 56;
+var publicKey = 'QGy3xKqXmySZy3QX1lgj'
+score
+po$t.on('click',function(){
+    $.ajax({
+      url: `http://data.sparkfun.com/input/${publicKey}?private_key=${priv_key}&name=${name}&score=${score}
+`,
+      method: "POST"
+    }).done((response) => {
+      console.log(response);
+
+      })
+})
 
