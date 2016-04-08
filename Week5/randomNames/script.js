1f0b77c546cf2b78b3b001da72b7534b7aa803ca
ilikealotgithub1234 <erikpmurdock@gmail.com>
2016-04-08 18:32:43 -0400

    Update script.js

--- Week5/randomNames/script.js
+++ Week5/randomNames/script.js
@@ -15,7 +15,7 @@ $("#getNames").click(function(){
     $("ol").append(`<li>${results}</li>`);
 });
 (
-*/
+
 var getName$ = function(url){
   var results = $.ajax({url: `${url}`});
   return results;
@@ -26,3 +26,28 @@ $("#getNames").click(function(){
 	    $("ol").append(`<li>${results}</li>`);
 	});
 });
+*/
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+var findnames = $.ajax({
+  url: 'https://randomuser.me/api/',
+  dataType: 'json',
+  success: function(results){
+      var foo = results.user;
+      console.log(results);
+      var title = capitalizeFirstLetter(results.results[0].name.title)+'.';
+      var fn = capitalizeFirstLetter(results.results[0].name.first);
+      var ln = capitalizeFirstLetter(results.results[0].name.last);
+      console.log(title,fn,ln)
+      var fullname = title + fn + ln;
+      return results.results;
+
+  }
+});
+var names = findnames
+var myFunction = () =>{
+  var fullname = findnames.fullname;
+  console.log(findnames);
+
+}
